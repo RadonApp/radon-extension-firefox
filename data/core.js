2f16470ffd390577f3fe6d8bbf0f4f84fc172e9f
Dean Gardiner <gardiner91@gmail.com>
2015-05-17 18:12:25 +1200

    Bumped version to 0.8.1-beta

--- data/core.js
+++ data/core.js
@@ -30,7 +30,7 @@ var EventHelper = (function() {
 })();
 
 var GMS = (function(port) {
-    this.version_number = [0, 8, 0];
+    this.version_number = [0, 8, 1];
     this.version_branch = 'beta';
     this.version = this.version_number.join('.') + '-' + this.version_branch;
 
