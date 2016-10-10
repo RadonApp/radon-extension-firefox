34b52bfea958325b28ddf256c73fd96014eb4ca4
Dean Gardiner <me@dgardiner.net>
2016-10-11 10:43:11 +1300

    Bumped version to 0.10.0-beta

--- data/core.js
+++ data/core.js
@@ -30,7 +30,7 @@ var EventHelper = (function() {
 })();
 
 var GMS = (function(port) {
-    this.version_number = [0, 9, 0];
+    this.version_number = [0, 10, 0];
     this.version_branch = 'beta';
     this.version = this.version_number.join('.') + '-' + this.version_branch;
 
