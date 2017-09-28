4f33e4fc2c3f14c2ae00bace066083f143e659dd
Dean Gardiner <me@dgardiner.net>
2017-09-28 17:26:40 +1300

    Hybrid extension version is now automatically set on build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,10 +1,11 @@
-import del from 'del';
-import fs from 'fs';
-import gulp from 'gulp';
-import gutil from 'gulp-util';
-import gzip from 'gulp-zip';
-import path from 'path';
-import rename from 'gulp-rename';
+import Delete from 'del';
+import Filesystem from 'fs';
+import Gulp from 'gulp';
+import GulpUtil from 'gulp-util';
+import GZip from 'gulp-zip';
+import Mkdirp from 'mkdirp';
+import Path from 'path';
+import Rename from 'gulp-rename';
 import {exec} from 'child_process';
 
 import Assets from './src/assets';
@@ -16,24 +17,24 @@ import Webpack from './src/webpack';
 import {buildDistributionName} from './src/core/helpers';
 
 
-gulp.task('build', ['build:production']);
+Gulp.task('build', ['build:production']);
 
 // region build:production
 
-gulp.task('build:production', [
+Gulp.task('build:production', [
     'clean:production',
 
     'webextension:production',
     'hybrid:production'
 ]);
 
-gulp.task('clean:production', () => {
-    return del([
+Gulp.task('clean:production', () => {
+    return Delete([
         __dirname + '/build/production/**/*'
     ]);
 });
 
-gulp.task('webextension:production:discover', ['clean:production'], (done) => {
+Gulp.task('discover:production', ['clean:production'], (done) => {
     // Discover modules
     Registry.discover().then(() => {
         done();
@@ -42,22 +43,22 @@ gulp.task('webextension:production:discover', ['clean:production'], (done) => {
     });
 });
 
-gulp.task('webextension:production', [
+Gulp.task('webextension:production', [
     'clean:production',
-    'webextension:production:discover',
+    'discover:production',
     'webextension:production:assets',
     'webextension:production:manifest',
     'webextension:production:package'
 ], () => {
     // Create archive of build
-    return gulp.src(path.join(Constants.BuildDirectory.Production.Unpacked, '**/*'))
-        .pipe(gzip(buildDistributionName(Extension.version)))
-        .pipe(gulp.dest(Constants.BuildDirectory.Production.Root));
+    return Gulp.src(Path.join(Constants.BuildDirectory.Production.Unpacked, '**/*'))
+        .pipe(GZip(buildDistributionName(Extension.version)))
+        .pipe(Gulp.dest(Constants.BuildDirectory.Production.Root));
 });
 
-gulp.task('webextension:production:assets', [
+Gulp.task('webextension:production:assets', [
     'clean:production',
-    'webextension:production:discover'
+    'discover:production'
 ], (done) => {
     Assets.build().then(
         () => done(),
@@ -65,9 +66,9 @@ gulp.task('webextension:production:assets', [
     );
 });
 
-gulp.task('webextension:production:manifest', [
+Gulp.task('webextension:production:manifest', [
     'clean:production',
-    'webextension:production:discover'
+    'discover:production'
 ], (done) => {
     Manifest.build().then(
         () => done(),
@@ -75,51 +76,80 @@ gulp.task('webextension:production:manifest', [
     );
 });
 
-gulp.task('webextension:production:package', [
+Gulp.task('webextension:production:package', [
     'clean:production',
-    'webextension:production:discover'
+    'discover:production'
 ], (callback) => {
     Webpack.build().then(
         (stats) => {
-            gutil.log(stats.toString('normal'));
+            GulpUtil.log(stats.toString('normal'));
             callback();
         },
         callback
     );
 });
 
-gulp.task('hybrid:production', [
+Gulp.task('hybrid:production', [
     'clean:production',
     'hybrid:production:package',
     'hybrid:production:xpi'
 ]);
 
-gulp.task('hybrid:production:package', [
+Gulp.task('hybrid:production:package', [
     'clean:production',
     'hybrid:production:wrapper',
     'hybrid:production:webextension'
 ], () => {
     // Create archive of build
-    return gulp.src(path.join(Constants.BuildDirectory.Production.Hybrid, '**/*'))
-        .pipe(gzip(buildDistributionName(Extension.version, {
+    return Gulp.src(Path.join(Constants.BuildDirectory.Production.Hybrid, '**/*'))
+        .pipe(GZip(buildDistributionName(Extension.version, {
             type: 'hybrid'
         })))
-        .pipe(gulp.dest(Constants.BuildDirectory.Production.Root));
+        .pipe(Gulp.dest(Constants.BuildDirectory.Production.Root));
 });
 
-gulp.task('hybrid:production:wrapper', ['clean:production'], () => {
+Gulp.task('hybrid:production:manifest', [
+    'clean:production',
+    'discover:production'
+], (done) => {
+    let manifest = JSON.parse(Filesystem.readFileSync('src/hybrid/package.json'));
+
+    // Set manifest version
+    manifest.version = Extension.version;
+
+    // Ensure destination directory exists
+    Mkdirp(Constants.BuildDirectory.Production.Hybrid);
+
+    // Build destination path
+    let destinationPath = Path.join(Constants.BuildDirectory.Production.Hybrid, 'package.json');
+
+    // Write manifest to build directory
+    Filesystem.writeFile(destinationPath, JSON.stringify(manifest, null ,2), function(err) {
+        if(err) {
+            done(err);
+            return;
+        }
+
+        done();
+    });
+});
+
+Gulp.task('hybrid:production:wrapper', [
+    'clean:production',
+    'hybrid:production:manifest'
+], () => {
     // Copy wrapper files
-    return gulp.src('src/hybrid/**/*')
-        .pipe(gulp.dest(Constants.BuildDirectory.Production.Hybrid));
+    return Gulp.src('src/hybrid/**/*.js')
+        .pipe(Gulp.dest(Constants.BuildDirectory.Production.Hybrid));
 });
 
-gulp.task('hybrid:production:webextension', ['webextension:production'], () => {
+Gulp.task('hybrid:production:webextension', ['webextension:production'], () => {
     // Copy production build
-    return gulp.src(path.join(Constants.BuildDirectory.Production.Unpacked, '**/!(*.map)'))
-        .pipe(gulp.dest(path.join(Constants.BuildDirectory.Production.Hybrid, 'webextension')));
+    return Gulp.src(Path.join(Constants.BuildDirectory.Production.Unpacked, '**/!(*.map)'))
+        .pipe(Gulp.dest(Path.join(Constants.BuildDirectory.Production.Hybrid, 'webextension')));
 });
 
-gulp.task('hybrid:production:xpi:build', ['hybrid:production:package'], (done) => {
+Gulp.task('hybrid:production:xpi:build', ['hybrid:production:package'], (done) => {
     // Create xpi of build
     exec('jpm xpi', { cwd: Constants.BuildDirectory.Production.Hybrid }, function (err, stdout, stderr) {
         console.log(stdout);
@@ -128,40 +158,34 @@ gulp.task('hybrid:production:xpi:build', ['hybrid:production:package'], (done) =
     });
 });
 
-gulp.task('hybrid:production:xpi', ['hybrid:production:xpi:build'], () => {
-    // Read extension manifest
-    let manifest = JSON.parse(fs.readFileSync(path.join(
-        Constants.BuildDirectory.Production.Hybrid,
-        'webextension/manifest.json'
-    )));
-
+Gulp.task('hybrid:production:xpi', ['hybrid:production:xpi:build'], () => {
     // Copy xpi to build directory
-    return gulp.src(path.join(Constants.BuildDirectory.Production.Hybrid, '*.xpi'))
-        .pipe(rename(buildDistributionName(manifest.version, {
+    return Gulp.src(Path.join(Constants.BuildDirectory.Production.Hybrid, '*.xpi'))
+        .pipe(Rename(buildDistributionName(Extension.version, {
             extension: 'xpi',
             type: 'hybrid'
         })))
-        .pipe(gulp.dest(Constants.BuildDirectory.Production.Root));
+        .pipe(Gulp.dest(Constants.BuildDirectory.Production.Root));
 });
 
 // endregion
 
 // region build:development
 
-gulp.task('build:development', [
+Gulp.task('build:development', [
     'clean:development',
 
     'webextension:development',
     'hybrid:development'
 ]);
 
-gulp.task('clean:development', () => {
-    return del([
+Gulp.task('clean:development', () => {
+    return Delete([
         __dirname + '/build/development/**/*'
     ]);
 });
 
-gulp.task('webextension:development:discover', ['clean:development'], (done) => {
+Gulp.task('discover:development', ['clean:development'], (done) => {
     // Discover modules
     Registry.discover('development').then(() => {
         done();
@@ -170,24 +194,24 @@ gulp.task('webextension:development:discover', ['clean:development'], (done) => 
     });
 });
 
-gulp.task('webextension:development', [
+Gulp.task('webextension:development', [
     'clean:development',
-    'webextension:development:discover',
+    'discover:development',
     'webextension:development:assets',
     'webextension:development:manifest',
     'webextension:development:package'
 ], () => {
     // Create archive of build
-    return gulp.src(path.join(Constants.BuildDirectory.Development.Unpacked, '**/*'))
-        .pipe(gzip(buildDistributionName(Extension.version, {
+    return Gulp.src(Path.join(Constants.BuildDirectory.Development.Unpacked, '**/*'))
+        .pipe(GZip(buildDistributionName(Extension.version, {
             environment: 'dev'
         })))
-        .pipe(gulp.dest(Constants.BuildDirectory.Development.Root));
+        .pipe(Gulp.dest(Constants.BuildDirectory.Development.Root));
 });
 
-gulp.task('webextension:development:assets', [
+Gulp.task('webextension:development:assets', [
     'clean:development',
-    'webextension:development:discover'
+    'discover:development'
 ], (done) => {
     Assets.build('development').then(
         () => done(),
@@ -195,9 +219,9 @@ gulp.task('webextension:development:assets', [
     );
 });
 
-gulp.task('webextension:development:manifest', [
+Gulp.task('webextension:development:manifest', [
     'clean:development',
-    'webextension:development:discover'
+    'discover:development'
 ], (done) => {
     Manifest.build('development').then(
         () => done(),
@@ -205,52 +229,81 @@ gulp.task('webextension:development:manifest', [
     );
 });
 
-gulp.task('webextension:development:package', [
+Gulp.task('webextension:development:package', [
     'clean:development',
-    'webextension:development:discover'
+    'discover:development'
 ], (callback) => {
     Webpack.build('development').then(
         (stats) => {
-            gutil.log(stats.toString('normal'));
+            GulpUtil.log(stats.toString('normal'));
             callback();
         },
         callback
     );
 });
 
-gulp.task('hybrid:development', [
+Gulp.task('hybrid:development', [
     'clean:development',
     'hybrid:development:package',
     'hybrid:development:xpi'
 ]);
 
-gulp.task('hybrid:development:package', [
+Gulp.task('hybrid:development:package', [
     'clean:development',
     'hybrid:development:wrapper',
     'hybrid:development:webextension'
 ], () => {
     // Create archive of build
-    return gulp.src(path.join(Constants.BuildDirectory.Development.Hybrid, '**/*'))
-        .pipe(gzip(buildDistributionName(Extension.version, {
+    return Gulp.src(Path.join(Constants.BuildDirectory.Development.Hybrid, '**/*'))
+        .pipe(GZip(buildDistributionName(Extension.version, {
             environment: 'dev',
             type: 'hybrid'
         })))
-        .pipe(gulp.dest(Constants.BuildDirectory.Development.Root));
+        .pipe(Gulp.dest(Constants.BuildDirectory.Development.Root));
 });
 
-gulp.task('hybrid:development:wrapper', ['clean:development'], () => {
+Gulp.task('hybrid:development:manifest', [
+    'clean:development',
+    'discover:development'
+], (done) => {
+    let manifest = JSON.parse(Filesystem.readFileSync('src/hybrid/package.json'));
+
+    // Set manifest version
+    manifest.version = Extension.version;
+
+    // Ensure destination directory exists
+    Mkdirp(Constants.BuildDirectory.Development.Hybrid);
+
+    // Build destination path
+    let destinationPath = Path.join(Constants.BuildDirectory.Development.Hybrid, 'package.json');
+
+    // Write manifest to build directory
+    Filesystem.writeFile(destinationPath, JSON.stringify(manifest, null ,2), function(err) {
+        if(err) {
+            done(err);
+            return;
+        }
+
+        done();
+    });
+});
+
+Gulp.task('hybrid:development:wrapper', [
+    'clean:development',
+    'hybrid:development:manifest'
+], () => {
     // Copy wrapper files
-    return gulp.src('src/hybrid/**/*')
-        .pipe(gulp.dest(Constants.BuildDirectory.Development.Hybrid));
+    return Gulp.src('src/hybrid/**/*.js')
+        .pipe(Gulp.dest(Constants.BuildDirectory.Development.Hybrid));
 });
 
-gulp.task('hybrid:development:webextension', ['webextension:development'], () => {
+Gulp.task('hybrid:development:webextension', ['webextension:development'], () => {
     // Copy development build
-    return gulp.src(path.join(Constants.BuildDirectory.Development.Unpacked, '**/*'))
-        .pipe(gulp.dest(path.join(Constants.BuildDirectory.Development.Hybrid, 'webextension')));
+    return Gulp.src(Path.join(Constants.BuildDirectory.Development.Unpacked, '**/*'))
+        .pipe(Gulp.dest(Path.join(Constants.BuildDirectory.Development.Hybrid, 'webextension')));
 });
 
-gulp.task('hybrid:development:xpi:build', ['hybrid:development:package'], (done) => {
+Gulp.task('hybrid:development:xpi:build', ['hybrid:development:package'], (done) => {
     // Create xpi of build
     exec('jpm xpi', { cwd: Constants.BuildDirectory.Development.Hybrid }, function (err, stdout, stderr) {
         console.log(stdout);
@@ -259,15 +312,15 @@ gulp.task('hybrid:development:xpi:build', ['hybrid:development:package'], (done)
     });
 });
 
-gulp.task('hybrid:development:xpi', ['hybrid:development:xpi:build'], () => {
+Gulp.task('hybrid:development:xpi', ['hybrid:development:xpi:build'], () => {
     // Copy xpi to build directory
-    return gulp.src(path.join(Constants.BuildDirectory.Development.Hybrid, '*.xpi'))
-        .pipe(rename(buildDistributionName(Extension.version, {
+    return Gulp.src(Path.join(Constants.BuildDirectory.Development.Hybrid, '*.xpi'))
+        .pipe(Rename(buildDistributionName(Extension.version, {
             environment: 'dev',
             extension: 'xpi',
             type: 'hybrid'
         })))
-        .pipe(gulp.dest(Constants.BuildDirectory.Development.Root));
+        .pipe(Gulp.dest(Constants.BuildDirectory.Development.Root));
 });
 
 // endregion
