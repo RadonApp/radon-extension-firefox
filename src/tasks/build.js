09491cc09d9d8b7f2d2903dd2a51972e009e6b8e
Dean Gardiner <me@dgardiner.net>
2017-10-05 12:19:18 +1300

    Implemented build checksum generation

--- src/tasks/build.js
+++ src/tasks/build.js
@@ -1,13 +1,32 @@
 import Gulp from 'gulp';
+import GulpHashsum from 'gulp-hashsum';
+import Path from 'path';
 
+import Constants from '../core/constants';
 import {getTaskName} from '../core/helpers';
 
 
 export function createTask(environment) {
-    Gulp.task(getTaskName(environment, 'build'), [
+    Gulp.task(getTaskName(environment, 'build:package'), [
         getTaskName(environment, 'clean'),
         getTaskName(environment, 'extension'),
         getTaskName(environment, 'hybrid')
+    ]);
+
+    Gulp.task(getTaskName(environment, 'build:hashsum'), [
+        getTaskName(environment, 'build:package')
+    ], () => {
+        // Create checksum file
+        return Gulp.src(Path.join(Constants.BuildDirectory.Root, environment, '*.{xpi,zip}'))
+            .pipe(GulpHashsum({
+                dest: Path.join(Constants.BuildDirectory.Root, environment),
+                hash: 'md5'
+            }));
+    });
+
+    Gulp.task(getTaskName(environment, 'build'), [
+        getTaskName(environment, 'build:package'),
+        getTaskName(environment, 'build:hashsum')
     ]);
 }
 
